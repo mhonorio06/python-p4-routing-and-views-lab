86c99857496d835ab393abbee79d7a10b8d38ece
Manny <mhfernan35277@gmail.com>
2024-09-01 02:22:00 -0700

    add file

--- server/app.py
+++ server/app.py
@@ -4,5 +4,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    
+    print(f'{parameter}')
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    count = f''
+    for x in range(parameter):
+        count += f'{x}\n'
+    return count
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    if operation == "+":
+        return str(num1 + num2)
+    elif operation == "-":
+        return str(num1 - num2)
+    elif operation == "*":
+        return str(num1 * num2)
+    elif operation == "div":
+        return str(num1 / num2)
+    elif operation == "%":
+        return str(num1 % num2)
+    else:
+        return 'Invalid Choice: Operation not recognized!'
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
